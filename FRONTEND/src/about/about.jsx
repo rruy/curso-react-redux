84d2a6d9ac77a6628cc6ead09bbe2e15fc30ddb8
Ricardo Ruy <ricardo.ruy@ext.smartfit.com.br>
2020-01-05 01:35:54 -0300

    Implementação com React + NodeJs + Express

---
+++ FRONTEND/src/about/about.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default class About extends Component {
+    render() {
+        return (
+          <div>
+            <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+
+              <h2>fwefewfwefwe</h2>
+              <p>efwefweifulghewiufwe weiufg ewiuf geqwiufg weiuf</p>
+
+              <h2>fwefewfwefwe</h2>
+              <p>efwefweifulghewiufwe weiufg ewiuf geqwiufg weiuf</p>
+
+              <h2>fwefewfwefwe</h2>
+              <p>efwefweifulghewiufwe weiufg ewiuf geqwiufg weiuf</p>
+          </div>
+        )
+    }
+}
